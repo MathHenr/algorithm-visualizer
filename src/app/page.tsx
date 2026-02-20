2d22e6591ec09235710f8a4729f7b200655ceb5d
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-20 20:24:02 -0300

    chore: init content component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Content } from "@/components/Content";
 import { Sidebar } from "@/components/Sidebar";
 
 export default function Home() {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+      <Content />
+    </>
+  );
 }
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,3 @@
+export function Content() {
+  return <h1 className="w-full mx-auto bg-red-500">Conteúdo</h1>;
+}
