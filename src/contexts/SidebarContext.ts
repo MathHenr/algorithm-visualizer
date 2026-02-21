8db9a9823ce8569bcd2b5ce939c4dc94f7449fd9
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-21 00:39:29 -0300

    feat: created sidebar context for better manage responsive functionality.

---
+++ src/contexts/SidebarContext.ts
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+type SidebarContextType = {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SidebarInitialState = {
+  isOpen: false,
+  setIsOpen: () => {},
+};
+
+export const SidebarContext =
+  createContext<SidebarContextType>(SidebarInitialState);
