99e0998c2582c279313d10d3fa4874b57e845bd6
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-23 20:25:59 -0300

    feat: created my own array class.

---
+++ src/core/array-structure.ts
@@ -0,0 +1,43 @@
+import { AnimationStep } from "@/types/animation";
+
+export class ArrayStructure<T> {
+  private items: Array<T | null>;
+
+  private constructor(size: number) {
+    this.items = new Array(size).fill(null);
+  }
+
+  // Implementing a visual linear search
+  find(value: T): { steps: AnimationStep[] } {
+    const steps: AnimationStep[] = [];
+
+    for (let i = 0; i < this.items.length; i++) {
+      steps.push({
+        type: "VISIT",
+        payload: { index: i },
+        message: `Checking index ${i}`,
+      });
+
+      if (this.items[i] === value) {
+        steps.push({
+          type: "FOUND",
+          payload: { index: i },
+          message: `Value found at index ${i}`,
+        });
+
+        return { steps };
+      }
+    }
+    steps.push({ type: "ERRORS", payload: null, message: "Value not found" });
+    return { steps };
+  }
+
+  update(index: number, value: T) {
+    this.items[index] = value;
+    return [...this.items];
+  }
+
+  get() {
+    return [...this.items];
+  }
+}
