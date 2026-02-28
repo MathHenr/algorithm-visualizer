d1a02c742be01ad803e1f5c386942c374f1f2f2f
Matheus Henrique Sobrinho <matheusobrinho2k20@gmail.com>
2026-02-28 16:49:48 -0300

    doc: lucide github and linkedin icons are depricated.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,22 @@
+import { GithubIcon, LinkedinIcon } from "lucide-react";
+import Link from "next/link";
+
 export function Footer() {
   return (
     <footer className="flex justify-center py-4">
-      <div className="flex flex-col items-center font-bold text-slate-800 text-sm">
-        <p>Este projeto foi feito com cunho educacional.</p>
-        <p>Criado e desenvolvido por Matheus.</p>
+      <div className="flex flex-col items-center">
+        <p className="text-center leading-5 sm:font-bold text-slate-800 text-sm">
+          Este projeto foi feito com cunho educacional.
+        </p>
+        <p className="text-center leading-5 sm:font-bold text-slate-800 text-sm">
+          Criado e desenvolvido por Matheus.
+          <Link href="#">
+            <GithubIcon />
+          </Link>
+          <Link href="#">
+            <LinkedinIcon />
+          </Link>
+        </p>
       </div>
     </footer>
   );
